50a3a7bb5665551779376a7a52cd0aa1e89066d0
JustenNicanor <92636904+JustenNicanor@users.noreply.github.com>
2023-02-16 02:03:28 +0800

    Add files via upload

---
+++ bubbleSort.py
@@ -0,0 +1,14 @@
+import time
+
+def bubble_sort(data, drawData):
+    for i in range(len(data)-1,0,-1):
+        for j in range(i):
+            if data[j] > data[j + 1]:
+                temp = data[j]
+                data[j] = data[j + 1]
+                data[j + 1] = temp
+                drawData(data, ['green' if x == j + 1 else 'yellow' for x in range(len(data))])
+                time.sleep(0.2)
+    drawData(data,  ['green' for x in range(len(data))])
+    
+
